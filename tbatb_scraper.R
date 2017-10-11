454bc130f83a7f3f043e3702b6477969be249e62
longhowlam <longhowlam@gmail.com>
2017-10-11 09:26:35 +0100

    using purrr to scrape multiple years

--- tbatb_scraper.R
+++ tbatb_scraper.R
@@ -5,172 +5,32 @@ library(dplyr)
 
 # see http://soapcentral.com/bb/recaps/index.php
 
+yearstartdates = ymd(030106, 040105, 050103, 060102, 070101, 080107, 090105, 100104, 110103, 120102, 130107, 140106, 150105, 160104)
 
-
-
-start = ymd(080107)
-outrecaps2008 = NULL
-
-for(i in 1:52)
-{
-  
-  dstring = format(start, format="%y%m%d")  
-  link = paste0("http://soapcentral.com/bb/recaps/2008/", dstring, ".php")
-  print(link)
-  out = read_html(link)
-  recaps = html_nodes(out, xpath = '//div[@id="news_article"]')  %>% html_text()
-  temp = tibble(recaps=recaps, date = start)
-  outrecaps2008 = bind_rows(outrecaps2008, temp)
-  start = start + 7
+scrapeBByear = function(start){
+  tmpout = NULL
+  for(i in 1:52)
+  {
+    
+    dstring = format(start, format="%y%m%d")  
+    link = paste0("http://soapcentral.com/bb/recaps/", year(start), "/", dstring, ".php")
+    print(link)
+    out = read_html(link)
+    recaps = html_nodes(out, xpath = '//div[@id="news_article"]')  %>% html_text()
+    temp = tibble(recaps = recaps, date = start)
+    tmpout = bind_rows(tmpout, temp)
+    start = start + 7
+  }
+  tmpout
 }
 
+BBrecaps = purrr::map_dfr(yearstartdates, scrapeBByear)
 
-
-start = ymd(090105)
-outrecaps2009 = NULL
-
-for(i in 1:52)
-{
-  
-  dstring = format(start, format="%y%m%d")  
-  link = paste0("http://soapcentral.com/bb/recaps/2009/", dstring, ".php")
-  print(link)
-  out = read_html(link)
-  recaps = html_nodes(out, xpath = '//div[@id="news_article"]')  %>% html_text()
-  temp = tibble(recaps=recaps, date = start)
-  outrecaps2009 = bind_rows(outrecaps2009, temp)
-  start = start + 7
-}
-
-start = ymd(100104)
-outrecaps2010 = NULL
-
-for(i in 1:52)
-{
-  
-  dstring = format(start, format="%y%m%d")  
-  link = paste0("http://soapcentral.com/bb/recaps/2010/", dstring, ".php")
-  print(link)
-  out = read_html(link)
-  recaps = html_nodes(out, xpath = '//div[@id="news_article"]')  %>% html_text()
-  temp = tibble(recaps=recaps, date = start)
-  outrecaps2010 = bind_rows(outrecaps2010, temp)
-  start = start + 7
-}
-
-
-
-
-start = ymd(110103)
-outrecaps2011 = NULL
-
-for(i in 1:52)
-{
-  
-  dstring = format(start, format="%y%m%d")  
-  link = paste0("http://soapcentral.com/bb/recaps/2011/", dstring, ".php")
-  print(link)
-  out = read_html(link)
-  recaps = html_nodes(out, xpath = '//div[@id="news_article"]')  %>% html_text()
-  temp = tibble(recaps=recaps, date = start)
-  outrecaps2011 = bind_rows(outrecaps2011, temp)
-  start = start + 7
-}
-
-
-
-start = ymd(120102)
-outrecaps2012 = NULL
-
-for(i in 1:52)
-{
-  
-  dstring = format(start, format="%y%m%d")  
-  link = paste0("http://soapcentral.com/bb/recaps/2012/", dstring, ".php")
-  print(link)
-  out = read_html(link)
-  recaps = html_nodes(out, xpath = '//div[@id="news_article"]')  %>% html_text()
-  temp = tibble(recaps=recaps, date = start)
-  outrecaps2012 = bind_rows(outrecaps2012, temp)
-  start = start + 7
-}
-
-
-
-start = ymd(130107)
-outrecaps2013 = NULL
-
-for(i in 1:52)
-{
-  
-  dstring = format(start, format="%y%m%d")  
-  link = paste0("http://soapcentral.com/bb/recaps/2013/", dstring, ".php")
-  print(link)
-  out = read_html(link)
-  recaps = html_nodes(out, xpath = '//div[@id="news_article"]')  %>% html_text()
-  temp = tibble(recaps=recaps, date = start)
-  outrecaps2013 = bind_rows(outrecaps2013, temp)
-  start = start + 7
-}
-
-
-start = ymd(140106)
-outrecaps2014 = NULL
-
-for(i in 1:52)
-{
-  
-  dstring = format(start, format="%y%m%d")  
-  link = paste0("http://soapcentral.com/bb/recaps/2014/", dstring, ".php")
-  print(link)
-  out = read_html(link)
-  recaps = html_nodes(out, xpath = '//div[@id="news_article"]')  %>% html_text()
-  temp = tibble(recaps=recaps, date = start)
-  outrecaps2014 = bind_rows(outrecaps2014, temp)
-  start = start + 7
-}
-
-
-
-start = ymd(150105)
-outrecaps2015 = NULL
-
-for(i in 1:52)
-{
-  
-  dstring = format(start, format="%y%m%d")  
-  link = paste0("http://soapcentral.com/bb/recaps/2015/", dstring, ".php")
-  print(link)
-  out = read_html(link)
-  recaps = html_nodes(out, xpath = '//div[@id="news_article"]')  %>% html_text()
-  temp = tibble(recaps=recaps, date = start)
-  outrecaps2015 = bind_rows(outrecaps2015, temp)
-  start = start + 7
-}
-
-
-start = ymd(160104)
-outrecaps2016 = NULL
-
-for(i in 1:52)
-{
- 
-  dstring = format(start, format="%y%m%d")  
-  link = paste0("http://soapcentral.com/bb/recaps/2016/", dstring, ".php")
-  print(link)
-  out = read_html(link)
-  recaps = html_nodes(out, xpath = '//div[@id="news_article"]')  %>% html_text()
-  temp = tibble(recaps=recaps, date = start)
-  outrecaps2016 = bind_rows(outrecaps2016, temp)
-  start = start + 7
-}
 
 start = ymd(170102)
 outrecaps2017 = NULL
-
 for(i in 1:42)
 {
-  
   dstring = format(start, format="%y%m%d")  
   link = paste0("http://soapcentral.com/bb/recaps/2017/", dstring, ".php")
   print(link)
@@ -183,15 +43,7 @@ for(i in 1:42)
 
 
 AllBB = bind_rows(
-  outrecaps2008,
-  outrecaps2009,
-  outrecaps2010,
-  outrecaps2011,
-  outrecaps2012,
-  outrecaps2013,
-  outrecaps2014,
-  outrecaps2015,
-  outrecaps2016,
+  BBrecaps,
   outrecaps2017
 )
 
